a37a428305734b04a9c4503b9a6b8ce68abd5ace
forestsource <forestsource@users.noreply.github.com>
2024-09-05 20:38:04 +0900

    add privacy policy

--- app/Footer.tsx
+++ app/Footer.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 
-const Footer = () => (
-    <footer className="bg-gray-800 text-white py-4 px-8 text-center fixed bottom-0 w-full">
-        <p>2024 Any Page Reader</p>
-    </footer>
-);
+const Footer = () => {
+    return (
+        <footer className="bg-gray-800 text-white py-4 px-8 text-center fixed bottom-0 w-full">
+            <p>2024 Any Page Reader</p>
+        </footer>
+    )
+};
 
 export default Footer;
